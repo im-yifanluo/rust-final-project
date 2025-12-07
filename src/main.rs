14f4e28f73285335045040fc210f99b032f4c577
Suhaan Khan <suhaankhanisme@gmail.com>
2025-12-07 11:26:18 -0600

    Added intiial CLI Functionality for Menu, Handling-Buying

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,67 @@ mod book;
 use crate::order::Order;
 use crate::book::Book;
 
+use rand::Rng; 
+use std::io; 
+
+fn print_menu() {
+    println!(); 
+    println!("===============================");
+    println!(" Order Book Menu ");
+    println!("===============================");
+    println!("What would you like to do today?"); 
+    println!(" 1) View Order Book \n 2) Buy <qty> <price> \n 3) Sell <qty> <price> \n 4) Quit\n"); 
+}
+
+fn handle_buy(book: &mut Book) {
+    let mut rng = rand::thread_rng(); 
+    let id: i32 = rng.gen_range(1..=100); 
+
+    let owner : &str = "User"; 
+
+    println!("Please enter order in format: <qty> <price>"); 
+    
+    let mut input: String = String::new(); 
+    println!(">>>");
+    io::stdin().read_line(&mut input).expect("Invalid Format, pelase enter in <qty> <price> (eg.: 3 50)"); 
+
+    let parts: Vec<&str> = input.trim().split_whitespace().collect(); 
+    if parts.len() != 2 {
+        println!("Invalid Format, pelase enter in <qty> <price> (eg.: 3 50)");
+        return; 
+    }
+
+    let quantity : i32 = parts[0].trim().parse().expect("Invalid Quantity");
+    let price : f64 = parts[1].trim().parse().expect("Invalid Price");
+
+    //must add owner functionality later
+    book.add_bid(id, owner.to_string(), price, quantity);
+
+    println!("Your buy order has been added!"); 
+}
+
 fn main() {
     // Test code for order
-    println!("Welcome to the Order Book designed and implemented by Yifan Luo and Suhaan Khan");
+    println!("Welcome to the Order Book designed and implemented by Suhaan Khan and Yifan Luo!");
 
     let mut new_order_book: Book = Book::new(
         "APPL".to_string(),
     );
 
     new_order_book.add_ask(1, "Yifan".to_string(), 267.44, 100);
+    
+    
+    print_menu(); 
+    let mut input: String = String::new(); 
+    println!(">>>");
+    io::stdin().read_line(&mut input).expect("Invalid Character Entered: Please enter a number from 1-4"); 
+
+    let num: i32 = input.trim().parse().expect("Invalid Character Entered: Please enter a number from 1-4"); 
+    match num {
+        1 => println!("You selected View Order Book!"), 
+        2 => handle_buy(&mut new_order_book),  
+        3 => println!("You selected Sell!"), 
+        4 => println!("See you later..."),
+        _ => println!("Invalid Character Entered: Please enter a number from 1-4"), 
+    }
 }
--- target/debug/deps/order_book.d
+++ target/debug/deps/order_book.d
@@ -1,6 +1,7 @@
-C:\Suhaan\Rust_Final_Project\rust-final-project\target\debug\deps\order_book.d: src\main.rs src\order.rs
+C:\Suhaan\CS128\Rust_Final_Project\rust-final-project\target\debug\deps\order_book.d: src\main.rs src\order.rs src\book.rs
 
-C:\Suhaan\Rust_Final_Project\rust-final-project\target\debug\deps\order_book.exe: src\main.rs src\order.rs
+C:\Suhaan\CS128\Rust_Final_Project\rust-final-project\target\debug\deps\order_book.exe: src\main.rs src\order.rs src\book.rs
 
 src\main.rs:
 src\order.rs:
+src\book.rs:
